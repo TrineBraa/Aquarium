5fac07ebb76062db2eaddd51c4bd2bb056b29d73
trinebra <157688079+trinebra@users.noreply.github.com>
2024-08-23 14:09:25 +0200

    Start up menu

--- Program.cs
+++ Program.cs
@@ -7,15 +7,29 @@ Welcome();
 
  void Welcome()
 {
-    Console.WriteLine("\n \t Welcome to your aquarium!");
-    Console.WriteLine("\n The aquarium is currently empty, maybe you should add some fish to it?");
-    Console.WriteLine("You have alot of options for fish you can have in the aquarium" +
-                      "\n we will list some different fish so you can add them into the aquarium, or not whatever you'd prefer.");
-    Console.WriteLine("\n\t are you ready? (Y/N)");
-    var StartupResponse = Console.ReadLine();
+    while (true)
+    {
+        Console.WriteLine("\n \t Welcome to your aquarium!");
+        Console.WriteLine("\n The aquarium is currently empty, maybe you should add some fish to it?");
+        Console.WriteLine("You have alot of options for fish you can have in the aquarium" +
+                          "\n we will list some different fish so you can add them into the aquarium, or not whatever you'd prefer.");
+        Console.WriteLine("\n\t are you ready? (Y/N)");
+        var StartupResponse = Console.ReadLine().ToLower();
 
-    if (StartupResponse == "Y"){}
+        if (StartupResponse == "y")
+        {
+
+        }
+        else if (StartupResponse == "n")
+        {
+            Console.WriteLine("shutting down");
+            Environment.Exit(404);
+        }
+        else
+        {
+            Console.WriteLine("Please input either Y for Yes or N for no.");
+        }
 
 
-
+    }
 }   
